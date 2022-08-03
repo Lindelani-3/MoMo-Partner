50e7814c9019aa405639d1a8e90ed15b93ef964b
Developer Delisa <delisamav3@gmail.com>
2022-08-03 14:49:06 +0200

    Implemented Basic Splash for SplashActivity

--- app/src/main/java/com/developerdiindy/momopartner/presentation/SplashActivity.java
+++ app/src/main/java/com/developerdiindy/momopartner/presentation/SplashActivity.java
@@ -2,15 +2,34 @@ package com.developerdiindy.momopartner.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.developerdiindy.momopartner.R;
+import com.developerdiindy.momopartner.presentation.home.HomeActivity;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        doSplash();
+    }
+
+    private void doSplash() {
+        final Intent i = new Intent(SplashActivity.this, HomeActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+
+                finish();
+            }
+        }, 2000);
     }
 }
