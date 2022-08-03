b402c5da61d6706f0feff7a380516229c96d0507
Developer Delisa <delisamav3@gmail.com>
2022-08-03 15:28:02 +0200

    Added Activities for: - Transfer Details - Request to Pay Details - Select Beneficiary

---
+++ app/src/main/java/com/developerdiindy/momopartner/presentation/transfer/SelectBeneficiaryActivity.java
@@ -0,0 +1,16 @@
+package com.developerdiindy.momopartner.presentation.transfer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.developerdiindy.momopartner.R;
+
+public class SelectBeneficiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_beneficiary);
+    }
+}
