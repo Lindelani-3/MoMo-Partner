051e1421c6722147179ba88be08506204d312204
Developer Delisa <delisamav3@gmail.com>
2022-08-04 13:30:12 +0200

    Added Navigation Tests For Login Activity - Login -> Register - Login -> Register -> Login

--- app/src/androidTest/java/com/developerdiindy/momopartner/presentation/appuser/LoginActivityTest.java
+++ app/src/androidTest/java/com/developerdiindy/momopartner/presentation/appuser/LoginActivityTest.java
@@ -1,19 +1,19 @@
 package com.developerdiindy.momopartner.presentation.appuser;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withHint;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.*;
 
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.Espresso;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.developerdiindy.momopartner.MainActivity;
 import com.developerdiindy.momopartner.R;
 
 import org.junit.Rule;
@@ -28,24 +28,43 @@ public class LoginActivityTest {
 
     @Test
     public void isActivityLaunched() {
-        ActivityScenario scenario = rule.getScenario();
-        Espresso.onView(ViewMatchers.withId(R.id.activity_login)).check(matches(isDisplayed()));
+        ActivityScenario<LoginActivity> scenario = rule.getScenario();
+        onView(withId(R.id.activity_login)).check(matches(isDisplayed()));
     }
 
     @Test
     public void isActivityItemsShown() {
-        Espresso.onView(ViewMatchers.withId(R.id.et_username_login)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
-        Espresso.onView(ViewMatchers.withId(R.id.et_password_login)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
-        Espresso.onView(ViewMatchers.withId(R.id.btn_reg_login)).check(matches(isDisplayed()));
-        Espresso.onView(ViewMatchers.withId(R.id.btn_login)).check(matches(isDisplayed()));
+        onView(withId(R.id.et_username_login)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.et_password_login)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.btn_reg_login)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_login)).check(matches(isDisplayed()));
     }
 
     @Test
     public void isItemsShowingCorrectText() {
-        Espresso.onView(ViewMatchers.withId(R.id.et_username_login)).check(matches(withHint(R.string.username_login)));
-        Espresso.onView(ViewMatchers.withId(R.id.et_password_login)).check(matches(withHint(R.string.password)));
-        Espresso.onView(ViewMatchers.withId(R.id.btn_reg_login)).check(matches(withText(R.string.register)));
-        Espresso.onView(ViewMatchers.withId(R.id.btn_login)).check(matches(withText(R.string.login)));
+        onView(withId(R.id.et_username_login)).check(matches(withHint(R.string.username_login)));
+        onView(withId(R.id.et_password_login)).check(matches(withHint(R.string.password)));
+        onView(withId(R.id.btn_reg_login)).check(matches(withText(R.string.register)));
+        onView(withId(R.id.btn_login)).check(matches(withText(R.string.login)));
+    }
+
+    @Test
+    public void isNavLoginToRegisterWorking() {
+        //val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.btn_reg_login)).perform(click());
+        onView(withId(R.id.activity_register)).check(matches(isDisplayed()));
+    }
+
+    /**
+     * Test both ways to navigate from SecondaryActivity to MainActivity
+     */
+    @Test
+    public void isNavLoginToRegisterToLoginWorking() {
+        onView(withId(R.id.btn_reg_login)).perform(click());
+        onView(withId(R.id.activity_register)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_login_reg)).perform(click()); // method 1
+        onView(withId(R.id.activity_login)).check(matches(isDisplayed()));
     }
 
 }
