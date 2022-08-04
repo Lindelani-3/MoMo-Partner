e6c625e2d6edd438ec2ebc41be594bbcc7fb4f07
Developer Delisa <delisamav3@gmail.com>
2022-08-04 14:08:29 +0200

    Implemented AppUser Unit Tests - Issue: Test Not Running

---
+++ app/src/test/java/com/developerdiindy/momopartner/business/model/AppUserTest.java
@@ -0,0 +1,32 @@
+package com.developerdiindy.momopartner.business.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+class AppUserTest {
+
+    public AppUser appUser;
+
+    @Before
+    public void setup(){
+        appUser = new AppUser();
+    }
+
+    @Test
+    public void testAppUserModel() {
+        appUser = new AppUser("Steve", "Rogers", "cap@email.com", "password");
+        appUser.setId(101L);
+        assertEquals(101L, appUser.getId());
+        appUser.setFirstName("Marc");
+        assertEquals("Marc", appUser.getFirstName());
+        appUser.setLastName("Spectre");
+        assertEquals("Spectre", appUser.getLastName());
+        appUser.setEmail("moon@email.com");
+        assertEquals("moon@email.com", appUser.getEmail());
+        appUser.setPassword("12345");
+        assertEquals("12345", appUser.getPassword());
+    }
+
+}
