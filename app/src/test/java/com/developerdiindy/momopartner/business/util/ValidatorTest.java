ce561a60f1c9f58d7e3468314306dbbf81b3a3dd
Developer Delisa <delisamav3@gmail.com>
2022-08-05 11:24:43 +0200

    Added Validator Test - Not Implemented yet

---
+++ app/src/test/java/com/developerdiindy/momopartner/business/util/ValidatorTest.java
@@ -0,0 +1,25 @@
+package com.developerdiindy.momopartner.business.util;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.jupiter.api.Assertions.*;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.developerdiindy.momopartner.presentation.appuser.LoginActivity;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+
+class ValidatorTest {
+
+    int isInputValid;
+
+    @Test
+    void test_checkUserInput_Login() {
+    }
+
+    @Test
+    void test_checkUserInput_Register() {
+    }
+}
