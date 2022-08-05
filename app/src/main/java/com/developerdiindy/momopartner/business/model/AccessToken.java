cac26fcd9825ac797d7a252b1de3ade5d8036dbd
Developer Delisa <delisamav3@gmail.com>
2022-08-05 14:19:37 +0200

    Added & Implemented AccessToken Model

---
+++ app/src/main/java/com/developerdiindy/momopartner/business/model/AccessToken.java
@@ -0,0 +1,38 @@
+package com.developerdiindy.momopartner.business.model;
+
+import java.util.Date;
+
+public class AccessToken {
+
+    private int userId;
+    private String token;
+    private Date dateCreated;
+
+    public AccessToken(String token) {
+        this.token = token;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
