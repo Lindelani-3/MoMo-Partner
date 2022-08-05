54fd6b944e2b1daa7b20451ef471af9babcb3d3f
Developer Delisa <delisamav3@gmail.com>
2022-08-05 17:38:40 +0200

    Added AccessTokenTest

---
+++ app/src/test/java/com/developerdiindy/momopartner/business/model/AccessTokenTest.java
@@ -0,0 +1,23 @@
+package com.developerdiindy.momopartner.business.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+class AccessTokenTest {
+
+    AccessToken accessToken;
+
+    @Test
+    void testAccessTokenModel() {
+        accessToken = new AccessToken("DJFKSo0083kC");
+        accessToken.setToken("NewToken");
+        accessToken.setUserId(101);
+        accessToken.setDateCreated(new Date(1999, 05, 03));     // toDo use proper date type
+        assertEquals("NewToken", accessToken.getToken());
+        assertEquals(101, accessToken.getUserId());
+        assertEquals(new Date(1999, 05, 03), accessToken.getDateCreated());
+    }
+}
