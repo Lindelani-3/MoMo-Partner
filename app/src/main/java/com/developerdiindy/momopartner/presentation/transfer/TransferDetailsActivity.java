ee706cab1e11f30db67077bb557d3ea1274696bd
Developer Delisa <delisamav3@gmail.com>
2022-08-10 11:17:52 +0200

    Edited TransferDetailsActivity - Use Case Mockup - Reference InputViews

--- app/src/main/java/com/developerdiindy/momopartner/presentation/transfer/TransferDetailsActivity.java
+++ app/src/main/java/com/developerdiindy/momopartner/presentation/transfer/TransferDetailsActivity.java
@@ -1,26 +1,39 @@
 package com.developerdiindy.momopartner.presentation.transfer;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.test.core.app.ActivityScenario;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.developerdiindy.momopartner.R;
-import com.developerdiindy.momopartner.presentation.home.HomeActivity;
 
 public class TransferDetailsActivity extends AppCompatActivity {
 
     Button btnTransfer;
+    EditText etExternalId, etAmount, etPartyId, etMyMessage, etPayeeNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer_details);
 
+        getUserInput();
         setupButtons();
+    }
+
+    private void getUserInput() {
+        setupInputViews();
+    }
+
+    private void setupInputViews() {
+        etExternalId = findViewById(R.id.et_transfer_externalId);
+        etAmount = findViewById(R.id.et_transfer_amount);
+        etPartyId = findViewById(R.id.et_transfer_payeePartyID);
+        etMyMessage = findViewById(R.id.et_transfer_payerMessage);
+        etPayeeNote = findViewById(R.id.et_transfer_payeeNote);
     }
 
     private void setupButtons() {
@@ -28,9 +41,28 @@ public class TransferDetailsActivity extends AppCompatActivity {
         btnTransfer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                doTransfer();
                 startActivity(new Intent(TransferDetailsActivity.this, TransferStatusActivity.class));
             }
         });
+    }
+
+    private void doTransfer() {
+        doCreateAccessToken_POST();
+        doTransfer_POST();
+        doGetTransferStatus_GET();
+    }
+
+    private void doGetTransferStatus_GET() {
+
+    }
+
+    private void doTransfer_POST() {
+
+    }
+
+    private void doCreateAccessToken_POST() {
+
     }
 }
 
