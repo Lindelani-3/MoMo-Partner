2f20e1b3f30eb89243da2bf54e3143c60aacedd0
Developer Delisa <delisamav3@gmail.com>
2022-08-10 15:01:28 +0200

    Added Database Handler Class

--- app/src/main/java/com/developerdiindy/momopartner/presentation/appuser/RegisterActivity.java
+++ app/src/main/java/com/developerdiindy/momopartner/presentation/appuser/RegisterActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.developerdiindy.momopartner.R;
@@ -31,7 +30,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void setupViewItems() {
-        etFullName = findViewById(R.id.et_firstname_register);
+        etFullName = findViewById(R.id.et_fullname_register);
         etEmail = findViewById(R.id.et_email_register);
         etPassword = findViewById(R.id.et_password_register);
         etConfirmPassword = findViewById(R.id.et_confirm_password_register);
